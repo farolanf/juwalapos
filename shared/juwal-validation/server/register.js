9b86cdadd261d76947a7ea92fd25094c4b14e2f6
Farolan Faisal <farolanfaisal@gmail.com>
2019-02-12 12:35:41 +0800

    uniqueEmail: add server validation

--- shared/juwal-validation/server/register.js
+++ shared/juwal-validation/server/register.js
@@ -1,15 +1,20 @@
 const validate = require('../validate')
 
-module.exports = {
-  email: {
-    presence: true,
-    email: true
-  },
-  password: {
-    presence: true,
-    length: {
-      minimum: 3,
-      maximum: 20
+module.exports = function(options) {
+  return {
+    email: {
+      presence: true,
+      email: true,
+      uniqueEmail: {
+        apos: options.uniqueEmail.apos
+      }
+    },
+    password: {
+      presence: true,
+      length: {
+        minimum: 3,
+        maximum: 20
+      }
     }
   }
 }
--- shared/juwal-validation/validators/uniqueEmail.js
+++ shared/juwal-validation/validators/uniqueEmail.js
@@ -2,12 +2,23 @@ const axios = require('axios')
 
 module.exports = function(email, options) {
   return new Promise(function(resolve, reject) {
-    axios.post(options.url, { email })
-      .then(res => {
-        res.data.ok ? resolve() : resolve('^Email sudah terdaftar')
+    if (options.url) {
+      axios.post(options.url, { email })
+        .then(res => {
+          res.data.ok ? resolve() : resolve('^Email sudah terdaftar')
+        })
+        .catch(error => {
+          reject(new Error(error.message))
+        })
+    } else if (options.apos) {
+      options.apos.users.safe.findOne({ email }, function(err, user) {
+        if (err) {
+          return reject(new Error(err))
+        }
+        user ? resolve('^Email sudah terdaftar') : resolve()
       })
-      .catch(error => {
-        reject(new Error(error.message))
-      })
+    } else {
+      throw new Error('Missing url|apos option')
+    }
   })
 }
